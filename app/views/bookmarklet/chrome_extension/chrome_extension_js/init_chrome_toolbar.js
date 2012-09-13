61fe4c62dce0ec78b8467baaf32988889612dd03
David <david@david-Latitude-E6400.(none)>
2012-09-13 13:18:47 -0700

    ui is extracted

--- app/views/bookmarklet/chrome_extension/chrome_extension_js/init_chrome_toolbar.js
+++ app/views/bookmarklet/chrome_extension/chrome_extension_js/init_chrome_toolbar.js
@@ -1,6 +1,9 @@
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('chrome_extension_js/whereJSisWrittenLocalChrome.js');
-(document.head||document.documentElement).appendChild(s);
-s.onload = function() {
-    s.parentNode.removeChild(s);
-};
+var scriptsToBeInjected = ["toolbar_ui.js","whereJSisWrittenLocalChrome.js"];
+for (i=0;i<scriptsToBeInjected.length;i++){
+    var s = document.createElement('script');
+    s.src = chrome.extension.getURL('chrome_extension_js/'+scriptsToBeInjected[i]);
+    (document.head||document.documentElement).appendChild(s);
+    s.onload = function(e) {
+        e.target.parentNode.removeChild(e.target);
+    };
+}
