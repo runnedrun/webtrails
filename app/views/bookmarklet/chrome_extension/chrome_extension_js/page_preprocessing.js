9649b91d9fa8088d3cdbb4c766519797a6a5ba24
Paul Booth <thephantompaulbooth@gmail.com>
2013-05-25 19:27:00 -0400

    No saving toolbar when save site

--- app/views/bookmarklet/chrome_extension/chrome_extension_js/page_preprocessing.js
+++ app/views/bookmarklet/chrome_extension/chrome_extension_js/page_preprocessing.js
@@ -1,12 +1,17 @@
 console.log("loaded pre-processing");
 
+// Edits in-place
+function removeToolbarFromPage($htmlClone) {
+  $htmlClone.find('.webtrails').remove();
+}
+
 function createMasterStyleSheet(html){
     var newStyleSheetString = "";
     wt_$(document.styleSheets).each(function(i,stylesheet){
         wt_$(stylesheet.rules).each(function(i,rule){
             newStyleSheetString += "\n" + rule.cssText;
-        })
-    })
+        });
+    });
     var masterStyleSheet = wt_$(document.createElement("style"));
     masterStyleSheet.className = "masterStylesheet";
     wt_$(html.getElementsByTagName("head")[0]).prepend(masterStyleSheet);
