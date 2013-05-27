37a6e6f3db852278f9ae447dfeef78b88de0d72a
Paul Booth <thephantompaulbooth@gmail.com>
2013-05-27 03:09:25 -0400

    make loading permalink view trail significantly faster

--- app/assets/javascripts/trail_display.js
+++ app/assets/javascripts/trail_display.js
@@ -42,12 +42,14 @@ function loadIframes(siteID){
 
 function makeIframes(){
     var currentSiteID = siteIDs[currentSiteIndex];
+    $('#' + currentSiteID).attr("src", requestUrl + "/sites/" + currentSiteID)
     loadIframes(currentSiteID);
     //site IDS defined in the html
     $.each(siteIDs,function (i,siteID){
         if (siteID != currentSiteID) {
-            console.log(siteID, currentSiteID, siteID == currentSiteID);
+            $('#' + siteID).attr("src", requestUrl + "/sites/" + siteID)
             loadIframes(siteID);
+
         }
     });
 }
@@ -182,7 +184,6 @@ function scrollToAndHighlightNote(noteID){
 
         var windowHeight = $(window).height();
         var scrollPosition = offsets.top - windowHeight/2;
-        console.log(scrollPosition);
         $(contWindow).scrollTop(scrollPosition);
 
         var commentDisplay = showComment(currentNote.comment,offsets.left,offsets.top);
